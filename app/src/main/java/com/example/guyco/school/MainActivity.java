d87fc2988268ad54a5d0524af32e0ccca58b451c
GuyCo5 <co.guycohen@gmail.com>
2018-09-30 00:08:32 +0300

    first commit menu and actionBar

--- app/src/main/java/com/example/guyco/school/MainActivity.java
+++ app/src/main/java/com/example/guyco/school/MainActivity.java
@@ -4,28 +4,39 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private static final String TAG = "main";
 
     private final String LINK_PHONEBOOK = "https://docs.google.com/spreadsheets/d/17FPrQvJD7oeCIfKJhq6jNkS3shfv_XudgoAViKF36Qs/edit?usp=sharing";
     private final String LINK_WHOIS = "https://he.wikipedia.org/wiki/%D7%90%D7%91%D7%A0%D7%A8_%D7%91%D7%A8%D7%96%D7%A0%D7%99";
     private final String LINK_EVENTS = "https://docs.google.com/document/d/1kIbqPBPw8_sKHw0XC-yBp8BY_rZqtS5ZH-v353UG6W8/edit?usp=sharing";
     private final String LINK_MESSAGES = "https://docs.google.com/document/d/1AMTO35iar_odnMq9cgOfuwV9MSot_vSrFMkGyoLhNeo/edit?usp=sharing";
     private final String LINK_GALLERY = "https://drive.google.com/drive/folders/17hTsO0oY8KUTS0QD9PdPf-finZtjTSo3?usp=sharing";
-    private final String LINK_SEND_TO_MANAGEMENT = "";
-    private final String LINK_SEND_TO_PARENTS = "";
-    private final String LINK_SEND_TO_NOONCARE = "";
+    //private final String LINK_SEND_TO_MANAGEMENT = "";
+    //private final String LINK_SEND_TO_PARENTS = "";
+    //private final String LINK_SEND_TO_NOONCARE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     public void clickPhoneBook(View view) {
